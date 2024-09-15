ef0e47882e921df673c4699c86623ef94fded107
cuenta1 <cuenta1udemy@gmail.com>
2024-09-15 16:26:00 -0500

    Commit creacion de Interfaces

---
+++ AlquilerVehicular/interfaces/IReserva.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlquilerVehicular.interfaces
+{
+    public interface IReserva
+    {
+        void Reservar(Cliente cliente, DateTime fechaInicio, DateTime fechaFin);
+        void Reservar();
+    }
+}
