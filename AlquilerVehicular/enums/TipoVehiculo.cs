7981cbd3049f47ec9b698c1ee2048884e1751601
cuenta1 <cuenta1udemy@gmail.com>
2024-09-15 16:58:52 -0500

    Commit Creacion de enums

---
+++ AlquilerVehicular/enums/TipoVehiculo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlquilerVehicular.enums
+{
+    public enum TipoVehiculo
+    {
+
+        AUTOMOVIL,
+        MOTOCICLETA,
+        CAMION
+    }
+}
