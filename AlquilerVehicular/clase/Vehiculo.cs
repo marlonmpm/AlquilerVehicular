de66a97516af5172b22718ed8678458b4b3609f6
MANUEL EMIR MURILLO ALEGR�A <manuelmurillo215106@correo.itm.edu.co>
2024-09-15 16:24:23 -0500

    feat: se crea clase abstracta Vehiculo

---
+++ AlquilerVehicular/clase/Vehiculo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaAlquilerVehiculos.@class;
+using SistemaAlquilerVehiculos.enums;
+using SistemaAlquilerVehiculos.interfaces;
+
+namespace SistemaAlquilerVehiculos.@class
+{
+
+    // Clase abstracta que representa un vehículo genérico.
+    public abstract class Vehiculo 
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Anio { get; set; }
+        public EstadoVehiculo Estado { get; set; }
+        public decimal PrecioAlquiler { get; private set; }
+        public abstract TipoVehiculo Tipo { get; set; }
+
+
+        public Vehiculo(string marca, string modelo, int anio, decimal precioAlquiler, TipoVehiculo tipo )
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Anio = anio;
+            Tipo = tipo;
+            PrecioAlquiler = precioAlquiler;
+            Estado = EstadoVehiculo.DISPONIBLE;
+        }
+
+
+
+        public void CambiarEstado(EstadoVehiculo nuevoEstado)
+        {
+            Estado = nuevoEstado;
+        }
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"Marca: {Marca}, Modelo: {Modelo}, Año: {Anio}, Precio Alquiler: {PrecioAlquiler}, Estado: {Estado}");
+        }
+
+
+      
+    }
+}
